d734423a9c553ac0f0fb61da1bd1db9d26a2158b
charb <charb@gmail.com>
2016-10-04 20:59:22 +0300

    pom changes

--- microsofia-container-test/runtest.bat
+++ microsofia-container-test/runtest.bat
@@ -1,1 +1,1 @@
-d:\java\jdk1.8.0_60\bin\java -jar ..\..\microsofia-boot\target\microsofia-boot.jar -c microsofia.container.Container -property:key4=0000 -property:key5=00000 -property:key6=000000
+d:\java\jdk1.8.0_60\bin\java -jar ..\..\microsofia-boot\target\microsofia-boot-1.0.jar -c microsofia.container.Container -property:key4=0000 -property:key5=00000 -property:key6=000000
